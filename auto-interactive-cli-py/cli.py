050f6abfc84501ae79b9d0b061c4aaa91e7db6f2
Delalande Romain <82338039+Delrom01@users.noreply.github.com>
2021-10-21 09:32:40 +0200

    Update cli.py

--- auto-interactive-cli-py/cli.py
+++ auto-interactive-cli-py/cli.py
@@ -1,54 +1,41 @@
 import questionary
 import inspect
-import commands    #fichier de commandes
+import commands    #commands.py file
 from colorama import *
 
 init()
 
 
 def cli():
-    #print("""--- Récupération des commandes (fonction) d'un module python ---""")
     commands_list = []
-    search_commands = inspect.getmembers(commands.Instances, inspect.isfunction)  # commands = fichier de commandes
-    #print(search_commands)
+    search_commands = inspect.getmembers(commands.Instances, inspect.isfunction)  # commands = commands.py file
     for i in range(len(search_commands)):
         if search_commands[i][0] == "__del__":
-            # print("""--- Suppression de la commande __del__ de la liste des commandes ---""")
             pass
         else:
             commands_list.append(search_commands[i][0])
-    #print(commands_list)
 
-
-    choice = questionary.select("Quelle commande voulez-vous exécuter ?", choices=commands_list).ask()
-
+    choice = questionary.select("Which command do you want to run ?", choices=commands_list).ask()
 
     if choice == "exit":
-        exit = questionary.confirm("Souhaitez-vous annuler 'exit' ?").ask()
+        exit = questionary.confirm("Do you want to cancel 'exit' ?").ask()
         print("")
         return choice, exit
 
     else:
-        #print("""--- Récupération des arguments associés à une commande (fonction) ---""")
         args = []
         params = inspect.getfullargspec(getattr(commands.Instances, choice))
-        #print(params)
         for arg in params.args:
             if arg == "self":
                 pass
             else:
                 args.append(arg)
-        #print(args)
-
 
         if not args:
-            #print("args ne contient pas d'arguments")
-            #print("""--- Appel de la commande ---""")
             print("")
             getattr(Instance, choice)()
             print("")
         else:
-            #print("""--- Récupération de la doc associée à une commande (fonction) ---""")
             doc = inspect.getdoc(getattr(commands.Instances, choice))
             print("")
             print("usage : commands.py [OPTION] COMMAND [ARGS]...")
@@ -56,30 +43,24 @@ def cli():
             print(doc)
             print("")
             args_values = [""]
-            #saisie = ""
             for i in range(len(args)):
-                saisie = questionary.text(f"Saisir une valeur pour l'argument {args[i]} : ").ask()
-                #saisie = input(f"Saisir une valeur pour l'argument {args[i]} : ")
-                args_values.append(saisie)
-            #print("Arguments saisies :")
-            #print(args_values)
-            #print("""--- Appel de la commande ---""")
+                entry = questionary.text(f"Enter a value for the argument {args[i]} : ").ask()
+                args_values.append(entry)
             print("")
             getattr(commands.Instances, choice)(*args_values)
             print("")
 
-
-        exit = questionary.confirm("Voulez-vous saisir une nouvelle commande ?").ask()
+        exit = questionary.confirm("Do you want to enter a new command ?").ask()
         print("")
         return choice, exit
 
 
 if __name__ == "__main__":
-    print(Fore.YELLOW + """--- DEBUT DU PROGRAMME ---""" + Fore.RESET)
+    print(Fore.YELLOW + """--- START OF PROGRAM ---""" + Fore.RESET)
     Instance = commands.Instances()
     print("")
-    saisie = cli()
-    while saisie[1] == True:
-        saisie = cli()
+    entry = cli()
+    while entry[1] == True:
+        entry = cli()
     del Instance
-    print(Fore.YELLOW + """--- FIN DU PROGRAMME ---""" + Fore.RESET)
+    print(Fore.YELLOW + """--- END OF PROGRAM ---""" + Fore.RESET)
